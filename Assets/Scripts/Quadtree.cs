5f3b9f94035151b41a6d6a273d7afeaa6d662dea
NamNH <hainam125@gmail.com>
2021-04-08 19:45:04 +0700

    Basic

---
+++ Assets/Scripts/Quadtree.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum QuadtreeIndex {
+    TopLeft = 0,        //00
+    TopRight = 1,       //01
+    BottomLeft = 2,     //10
+    BottomRight = 3,    //11
+}
+
+public class Quadtree<TType> : MonoBehaviour {
+    private QuadtreeNode<TType> node;
+    private int depth;
+
+    public Quadtree(Vector2 position, float size, int depth) {
+        node = new QuadtreeNode<TType>(position, size);
+        node.Subdivide(depth);
+    }
+
+    public QuadtreeNode<TType> GetRoot() => node;
+
+    private int GetIndexOfPosition(Vector2 lookupPosition, Vector2 nodePosition) {
+        int index = 0;
+        index |= lookupPosition.y > nodePosition.y ? 2 : 0;
+        index |= lookupPosition.x > nodePosition.x ? 1 : 0;
+        return index;
+    }
+}
+
+public class QuadtreeNode<TType> {
+    private Vector2 position;
+    private float size;
+    private QuadtreeNode<TType>[] subnodes;
+    private IList<TType> value;
+
+    public QuadtreeNode(Vector2 position, float size) {
+        this.position = position;
+        this.size = size;
+    }
+
+    public IEnumerable<QuadtreeNode<TType>> Nodes => subnodes;
+
+    public Vector3 Position => position;
+
+    public float Size => size;
+
+    public bool IsLeaf() => subnodes == null;
+
+    public void Subdivide(int depth = 0) {
+        subnodes = new QuadtreeNode<TType>[4];
+        for (int i = 0; i < subnodes.Length; i++) {
+            Vector2 newPos = position;
+            if ((i & 2) == 2) {
+                newPos.y -= size * 0.25f;
+            }
+            else {
+                newPos.y += size * 0.25f;
+            }
+            if ((i & 1) == 1) {
+                newPos.x += size * 0.25f;
+            }
+            else {
+                newPos.x -= size * 0.25f;
+            }
+            subnodes[i] = new QuadtreeNode<TType>(newPos, size * 0.5f);
+            if (depth > 0) {
+                subnodes[i].Subdivide(depth - 1);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/QuadtreeComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadtreeComponent : MonoBehaviour {
+    [SerializeField] private float size;
+    [SerializeField] private int depth;
+
+    private void OnDrawGizmos() {
+        var quadtree = new Quadtree<int>(transform.position, size, depth);
+        DrawNode(quadtree.GetRoot());
+    }
+
+    private Color minColor = new Color(1, 1, 1, 1);
+    private Color maxColor = new Color(0, 0.5f, 1, 0.25f);
+
+    private void DrawNode(QuadtreeNode<int> node, int nodeDepth = 0) {
+        if (!node.IsLeaf()) {
+            foreach (var subNode in node.Nodes) DrawNode(subNode, nodeDepth + 1);
+        }
+        Gizmos.color = Color.Lerp(minColor, maxColor, nodeDepth / (float)depth);
+        Gizmos.DrawWireCube(node.Position, new Vector3(1, 1, 0.1f) * node.Size);
+    }
+}
